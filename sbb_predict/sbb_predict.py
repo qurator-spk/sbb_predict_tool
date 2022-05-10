5b485bd62d558f658499753acbf273be81bd4694
vahid <vahid@linux-x3no.fritz.box>
2022-05-10 02:14:12 +0200

    uploading files

--- sbb_predict/sbb_predict.py
+++ sbb_predict/sbb_predict.py
@@ -14,11 +14,18 @@ import csv
 import cv2
 import seaborn as sns
 import matplotlib.pyplot as plt
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 import tensorflow as tf
-from keras import backend as K
+from tensorflow.keras import backend as K
+from tensorflow.keras import layers
+import tensorflow.keras.losses
+from tensorflow.keras.layers import *
 
-import keras.losses
+from tensorflow.python.keras import backend as tensorflow_backend
+
+
+
+
 
 
 with warnings.catch_warnings():
@@ -28,6 +35,62 @@ __doc__=\
 """
 Tool to load model and predict for given image.
 """
+
+projection_dim = 64
+patch_size = 1
+num_patches =28*28
+class Patches(layers.Layer):
+    def __init__(self, **kwargs):
+        super(Patches, self).__init__()
+        self.patch_size = patch_size
+
+    def call(self, images):
+        print(tf.shape(images)[1],'images')
+        print(self.patch_size,'self.patch_size')
+        batch_size = tf.shape(images)[0]
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size, self.patch_size, 1],
+            strides=[1, self.patch_size, self.patch_size, 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+        patch_dims = patches.shape[-1]
+        print(patches.shape,patch_dims,'patch_dims')
+        patches = tf.reshape(patches, [batch_size, -1, patch_dims])
+        return patches
+    def get_config(self):
+
+        config = super().get_config().copy()
+        config.update({
+            'patch_size': self.patch_size,
+        })
+        return config
+    
+    
+class PatchEncoder(layers.Layer):
+    def __init__(self, **kwargs):
+        super(PatchEncoder, self).__init__()
+        self.num_patches = num_patches
+        self.projection = layers.Dense(units=projection_dim)
+        self.position_embedding = layers.Embedding(
+            input_dim=num_patches, output_dim=projection_dim
+        )
+
+    def call(self, patch):
+        positions = tf.range(start=0, limit=self.num_patches, delta=1)
+        encoded = self.projection(patch) + self.position_embedding(positions)
+        return encoded
+    def get_config(self):
+
+        config = super().get_config().copy()
+        config.update({
+            'num_patches': self.num_patches,
+            'projection': self.projection,
+            'position_embedding': self.position_embedding,
+        })
+        return config
+    
 
 class sbb_predict:
     def __init__(self,image,model, patches='false',save='false', ground_truth=None,weights_dir=None ):
@@ -146,12 +209,21 @@ class sbb_predict:
             return mIoU
             
     def start_new_session_and_model(self):
-        config = tf.ConfigProto()
-        config.gpu_options.allow_growth=True
+        
+        config = tf.compat.v1.ConfigProto()
+        config.gpu_options.allow_growth = True
+
+        session = tf.compat.v1.Session(config=config)  # tf.InteractiveSession()
+        tensorflow_backend.set_session(session)
+        #tensorflow.keras.layers.custom_layer = PatchEncoder
+        #tensorflow.keras.layers.custom_layer = Patches
+        self.model = load_model(self.model_dir , compile=False,custom_objects = {"PatchEncoder": PatchEncoder, "Patches": Patches})
+        #config = tf.ConfigProto()
+        #config.gpu_options.allow_growth=True
     
-        self.session = tf.InteractiveSession()
-        keras.losses.custom_loss = self.weighted_categorical_crossentropy
-        self.model = load_model(self.model_dir , compile=False)
+        #self.session = tf.InteractiveSession()
+        #keras.losses.custom_loss = self.weighted_categorical_crossentropy
+        #self.model = load_model(self.model_dir , compile=False)
         
         if self.weights_dir!=None:
             print('man burdayammmmaaa')
